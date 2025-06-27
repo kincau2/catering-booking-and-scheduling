4b2e0487714604af8d02f1a4865f48a782dd7f4e
kincau2 <kincau2@hotmail.com>
2025-06-27 23:24:31 +0100

    2025-06-27

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,11 +11,6 @@ class ComposerStaticInitbf070d63e1b0ff6b62ddc4c4dcba3cb5
         array (
             'ZipStream\\' => 10,
         ),
-        'S' => 
-        array (
-            'Svg\\' => 4,
-            'Sabberworm\\CSS\\' => 15,
-        ),
         'P' => 
         array (
             'Psr\\SimpleCache\\' => 16,
@@ -26,15 +21,6 @@ class ComposerStaticInitbf070d63e1b0ff6b62ddc4c4dcba3cb5
         'M' => 
         array (
             'Matrix\\' => 7,
-            'Masterminds\\' => 12,
-        ),
-        'F' => 
-        array (
-            'FontLib\\' => 8,
-        ),
-        'D' => 
-        array (
-            'Dompdf\\' => 7,
         ),
         'C' => 
         array (
@@ -48,22 +34,14 @@ class ComposerStaticInitbf070d63e1b0ff6b62ddc4c4dcba3cb5
         array (
             0 => __DIR__ . '/..' . '/maennchen/zipstream-php/src',
         ),
-        'Svg\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/dompdf/php-svg-lib/src/Svg',
-        ),
-        'Sabberworm\\CSS\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/src',
-        ),
         'Psr\\SimpleCache\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/simple-cache/src',
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -76,18 +54,6 @@ class ComposerStaticInitbf070d63e1b0ff6b62ddc4c4dcba3cb5
         'Matrix\\' => 
         array (
             0 => __DIR__ . '/..' . '/markbaker/matrix/classes/src',
-        ),
-        'Masterminds\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/masterminds/html5/src',
-        ),
-        'FontLib\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/dompdf/php-font-lib/src/FontLib',
-        ),
-        'Dompdf\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/dompdf/dompdf/src',
         ),
         'Composer\\Pcre\\' => 
         array (
@@ -111,7 +77,6 @@ class ComposerStaticInitbf070d63e1b0ff6b62ddc4c4dcba3cb5
 
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-        'Dompdf\\Cpdf' => __DIR__ . '/..' . '/dompdf/dompdf/lib/Cpdf.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
